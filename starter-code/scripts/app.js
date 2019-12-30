c88218e2e58784af8e4528d6b3fd619cc73ed6fd
Fletch-7 <r.a.m.fletcher7@gmail.com>
2019-12-30 21:41:36 +0000

    progress

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -3,12 +3,11 @@ function init() {
 
   //  DOM Variables
   const grid = document.querySelector('.grid')
+  const start = document.querySelector('.start')
+  const reset = document.querySelector('.reset')
   const squares = []
-  const startStopBtn = document.querySelector('button')
  
   // Game Variables
-  let timerId = null // a variavble to store our interval id, we need to know this so we can stop it later (ticket at the coat check)
-  let running = false // a boolean value we use to determine if we should be stopping or starting the timer when the button is clicked, is it is set to false we need to start the interval, if it is true we need to stop it
   const width = 11 //121 squares
   let playerIndex = 0
 
@@ -21,74 +20,65 @@ function init() {
     grid.appendChild(square)
  
   })
-
-
-
+  squares[playerIndex].classList.add('player')
 
   
-  // squares[playerIndex].classList.add('player')
-  
-  // function handleKeyDown(e) { //KEYBOARD FUNCTION TO MOVE AROUND THE GRID 
-  //   console.log(playerIndex % width < width - 1)
-  //   // console.log(e.keyCode)
-  //   switch (e.keyCode){
-  //     case 39:
-  //       if (playerIndex % width < width - 1){
-  //         playerIndex++ //move by 1 everytime right
-  //       }
-  //       break
-  //     case 37:
-  //       if (playerIndex % width > 0){
-  //         playerIndex-- //move one by everytime left
-  //       }
-  //       break
-  //     case 40:
-  //       if (playerIndex + width < width * width){
-  //         playerIndex += width
-  //       }
-  //       break
-  //     case 38:
-  //       if (playerIndex - width >= 0 ){
-  //         playerIndex -= width
-  //       }
-  //       break
-  //     default: 
-  //       console.log('player shouldnt move')
-  //   }
-  //   squares.forEach(square => square.classList.remove('player'))
-  //   squares[playerIndex].classList.add('player')
+  function handleKeyDown(e) { //KEYBOARD FUNCTION TO MOVE AROUND THE GRID 
+    // console.log(playerIndex % width < width - 1)
+    // console.log(e.keyCode)
+    switch (e.keyCode){
+      case 39:
+        if (playerIndex % width < width - 1){
+          playerIndex++ //moves by one right
+        }
+        break
+      case 37:
+        if (playerIndex % width > 0){
+          playerIndex--//moves by one left
+        }
+        break
+      case 40:
+        if (playerIndex + width < width * width){
+          playerIndex += width//moves by one down
+        }
+        break
+      case 38:
+        if (playerIndex - width >= 0 ){
+          playerIndex -= width//moves by one up
+        }
+        break
+      default: 
+        console.log('player shouldnt move')
 
-  // }
-
-
-  //MOVEMENT SECTION
-
-  function changeSquareColor() {
-    squares.forEach(square => square.style.backgroundColor = 'grey') // first we change all the squares back to green
-    squares[playerIndex].style.backgroundColor = 'white' // then make the one single square (found at currentSquareIndex) red again
-    playerIndex++ // increase that current square index at the end, so it is ready for the next run
-    if (playerIndex >= squares.length) { // if we get to the end of the squares (in this case, our current square index reaches 5, the length of the squares array)
-      playerIndex = 0 // set that current square index back to zero, so on the next run, it is the first square changing again and so forth, this will just keep looping them until the interval is cleared
+        grid.snake = 1
     }
-  }
-  function handleStart() {
-    if (!running) { // is the running value was false (the interval wasnt running)  
-      timerId = setInterval(changeSquareColor, 1000) // start the interval, remember the syntax is, what function to run, and how often to run it, and we store the return id in a variable, so we can use it to stop the interval later
-      startStopBtn.innerHTML = 'Stop' // we could even change the text inside the button to relect what it will do on its next action
-      running = true // set the value of running to now be true, so when the button is next clicked, we know to stop the timer instead
-    } else { // so the else runs only is running was true
-      startStopBtn.innerHTML = 'Start' // when we stopped it, have the button say start again for the user
-      clearInterval(timerId) // and that is when we stop the timer
-    }
+    squares.forEach(square => square.classList.remove('player'))
+    squares[playerIndex].classList.add('player')
+
   }
 
-  startStopBtn.addEventListener('click', handleStart) // event listenter for the click on our start/stop button, runs handleStart() on click.
-  
-  //MOVEMENT SECTION
+  //FOOD SECTION
+  function generateFood (){
+    const randomNumbers = new Set() // create an empty set to push random numbers in to
+    while (randomNumbers.size < 1){
+      console.log('hello')
+      const randomNumber = Math.floor(Math.random() * 121) // generate a random number between 0 and 49
+      randomNumbers.add(randomNumber) // add randomNumber to the end of the randomNumbers set
+    }
+    randomNumbers.forEach((number) => { // loop through the randomNumbers set
+      squares[number].classList.add('active') // find the li with the index of the random number and add the class of "active"
+    })
+  }
+  //RESET SECTION
+  function clearGrid() {
+    location.reload()
+  }
 
-  
+
   //Event Listeners
-  // window.addEventListener('keydown', handleKeyDown)
+  window.addEventListener('keydown', handleKeyDown)
+  start.addEventListener('click', generateFood)
+  reset.addEventListener('click', clearGrid)
 }
 
 window.addEventListener('DOMContentLoaded', init)
