15c20903293536aae467dc5c346b7fd808590a4a
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-01-06 16:03:35 +0000

    day1/2

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,18 +1,14 @@
-
 function init() {
 
   //  DOM Variables
   const grid = document.querySelector('.grid')
-  const start = document.querySelector('.start')
   const reset = document.querySelector('.reset')
   const squares = []
-  let snakeArray = [0, 1, 2, 3].sort((a, b) => b - a)
-  let snakeHead = [0]
+  const snakeArray = [3, 2, 1, 0]
   let speed = 300
  
   // Game Variables
   const width = 11 //121 squares
-  
   let direction = 'right'
 
  
@@ -25,34 +21,31 @@ function init() {
   })
 
   function addSnake() {
-    snakeArray.map(snake => squares[snake].classList.add('snake'))
+    snakeArray.forEach(snake => squares[snake].classList.add('snake'))
   }
   function removeSnake(){
-    snakeArray.map(snake => squares[snake].classList.remove('snake'))
+    snakeArray.forEach(snake => squares[snake].classList.remove('snake'))
   }
   addSnake()
 
   function handleKeyDown(e) { //KEYBOARD FUNCTION TO MOVE AROUND THE GRID 
     switch (e.keyCode){
-      case 39: direction = 'right'//moves by one right
-        moveRight()
-        if (direction !== 'right')
-          direction === true
-        break
-      case 37: direction = 'left'//moves by one left
-        moveLeft()
+      case 39://moves by one right
         if (direction !== 'left')
-          direction === true
+          direction = 'right'
         break
-      case 40: direction = 'down'//moves by one down
-        moveDown()
-        if (direction !== 'down')
-          direction === 'up'
+      case 37: //moves by one left
+        if (direction !== 'right')
+          direction = 'left'
+
         break
-      case 38: direction = 'up'//moves by one up
-        moveUp()
+      case 40: //moves by one down
         if (direction !== 'up')
-          direction === 'down'
+          direction = 'down'
+        break
+      case 38: //moves by one up
+        if (direction !== 'down')
+          direction = 'up'
         break
       default: 
         console.log('player shouldnt move')
@@ -60,14 +53,13 @@ function init() {
 
   }
   // handleKeyDown()
-
-
   function moveSnake() {
     if (direction === 'right' && snakeArray[0] % width < width - 1) {
       moveRight()
     }
     if (direction === 'left' && snakeArray[0] % width > 0){
       moveLeft()
+      
     }
     if (direction === 'down' && snakeArray[0] + width < width * width){
       moveDown()
@@ -75,40 +67,86 @@ function init() {
     if (direction === 'up' && snakeArray[0] - width >= 0){
       moveUp()
     }
-  }
-
-
     
+  }
+ 
   function moveRight (){
+    snakeWall()
     removeSnake()
-    snakeArray.pop()
     snakeArray.unshift(snakeArray[0] + 1)
+    console.log(squares[snakeArray[0]].classList.contains('food'))
+    if (!squares[snakeArray[0]].classList.contains('food')) {
+      snakeArray.pop()
+    }
+    snakeEats()
     addSnake()
+    
   }
   function moveLeft(){
+    snakeWall()
     removeSnake()
-    snakeArray.pop()
     snakeArray.unshift(snakeArray[0] - 1)
+    console.log(squares[snakeArray[0]].classList.contains('food'))
+    if (!squares[snakeArray[0]].classList.contains('food')) {
+      snakeArray.pop()
+    }
+    snakeEats()
     addSnake()
+    
   }
   function moveDown(){
+    snakeWall()
     removeSnake()
-    snakeArray.pop()
     snakeArray.unshift(snakeArray[0] + width)
+    console.log(squares[snakeArray[0]].classList.contains('food'))
+    if (!squares[snakeArray[0]].classList.contains('food')) {
+      snakeArray.pop()
+    }
+    snakeEats()
     addSnake()
+   
   }
   function moveUp(){
+    snakeWall()
     removeSnake()
-    snakeArray.pop()
-    snakeArray.unshift(snakeArray[0] - width)
-    addSnake()
+    if (snakeArray[0] > 0) {
+      snakeArray.unshift(snakeArray[0] - width)
+      if (!squares[snakeArray[0]].classList.contains('food')) {
+        snakeArray.pop()
+      }
+      snakeEats()
+      addSnake()
+    }
   }
 
-    
-  
-  
   setInterval(moveSnake, speed)
   generateFood()
+  
+  
+
+  // Snake hits wall
+  function snakeWall(){
+    
+    if (snakeArray[0] === snakeArray[0] % width < width - 1 < 0 ){
+      window.alert('GAME OVER :(')
+      clearGrid()
+    } 
+    if (snakeArray[0] === snakeArray[0] % width > 0 ){
+      window.alert('GAME OVER :(')
+      clearGrid()
+    }
+    if (snakeArray[0] === snakeArray[0] + width < width * width){
+      window.alert('GAME OVER :(')
+      clearGrid()
+    }
+    if (snakeArray[0] === snakeArray[0] - width >= 0){
+      window.alert('GAME OVER :(')
+      clearGrid()
+    }
+  }
+ 
+  
+  
 
   //FOOD SECTION
   function generateFood (){
@@ -118,11 +156,29 @@ function init() {
       randomNumbers.add(randomNumber) // add randomNumber to the end of the randomNumbers set
     }
     randomNumbers.forEach((number) => { // loop through the randomNumbers set
-      squares[number].classList.add('active') // find the li with the index of the random number and add the class of "active"
+      squares[number].classList.add('food') // find the li with the index of the random number and add the class of "active"
     })
   }
 
   //Snake Eat Food
+  // function snakeEats(){
+  //   if (snakeArray[0] === 'active')
+  //     squares.classList.remove('active')
+  // }
+  // snakeEats()
+  //(squares[snakeArray[0]].classList.contains('food'))
+  function snakeEats(){
+    if (squares[snakeArray[0]].classList.contains('food')) { //square = singular of squares variable
+      squares[snakeArray[0]].classList.remove('food')//remove class -> remove color
+      generateFood()
+      speed = speed - 100
+    } 
+  }
+    
+
+  
+  
+
   
   //RESET SECTION
   function clearGrid() {
